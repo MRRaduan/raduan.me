ca1f902fa7878bb26963db977365a24e1caccdbe
Matheus Raduan <matheusraduan@Matheuss-MacBook-Air.local>
2022-08-08 20:30:48 -0300

    chore: config fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import GlobalStyles from 'styles/global'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>raduan.me - Hello!</title>
+        <link rel="shortcut icon" href="favicon.png" />
+        <link rel="apple-touch-icon" href="favicon.png" />
+        <meta
+          name="description"
+          content="I'm Matheus Raduan. Frontend Developer "
+        />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
-export default MyApp
+export default App
---
+++ styles/global.ts
@@ -0,0 +1,71 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'IBM Plex Sans';
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+    src: url('/fonts/ibm-plex-sans/ibm-plex-sans-v14-latin-regular.woff2') format('woff2'); 
+  }
+  @font-face {
+    font-family: 'IBM Plex Sans';
+    font-style: italic;
+    font-weight: 400;
+    font-display: swap;
+    src: url('/fonts/ibm-plex-sans/ibm-plex-sans-v14-latin-italic.woff2') format('woff2'); 
+  }
+  @font-face {
+    font-family: 'IBM Plex Sans';
+    font-style: normal;
+    font-weight: 600;
+    font-display: swap;
+    src: url('/fonts/ibm-plex-sans/ibm-plex-sans-v14-latin-600.woff2') format('woff2'); 
+  }
+  @font-face {
+    font-family: 'IBM Plex Serif';
+    font-style: normal;
+    font-weight: 400;
+    font-display: swap;
+    src: url('/fonts/ibm-plex-serif/ibm-plex-serif-v15-latin-regular.woff2') format('woff2'); 
+  }
+  @font-face {
+    font-family: 'IBM Plex Serif';
+    font-style: italic;
+    font-weight: 400;
+    font-display: swap;
+    src: url('/fonts/ibm-plex-serif/ibm-plex-serif-v15-latin-italic.woff2') format('woff2'); 
+  }
+  @font-face {
+    font-family: 'IBM Plex Serif';
+    font-style: normal;
+    font-weight: 600;
+    font-display: swap;
+    src: url('/fonts/ibm-plex-serif/ibm-plex-serif-v15-latin-600.woff2') format('woff2'); 
+  }
+  @font-face {
+    font-family: 'IBM Plex Serif';
+    font-style: italic;
+    font-weight: 600;
+    font-display: swap;
+    src: url('/fonts/ibm-plex-serif/ibm-plex-serif-v15-latin-600italic.woff2') format('woff2'); 
+  }
+
+  * {
+    margin:0;
+    padding: 0;
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    font-family: 'IBM Plex Sans', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', serif-serif;
+  }
+`
+
+export default GlobalStyles
