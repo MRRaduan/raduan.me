8497b3fffde42f702c9f5dbe4134e8f1ed7fe96e
Matheus Raduan <matheusraduan@Matheuss-MacBook-Air.local>
2022-11-16 21:33:07 -0300

    add about me content

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -35,6 +35,109 @@ const About = () => {
           </S.TitleWrapper>
         </S.TextWrapper>
       </S.Wrapper>
+      <S.AboutMeContent>
+        <S.AboutContentWrapper>
+          <S.Quote>
+            Everything is designed. <br />
+            Few things are designed well
+          </S.Quote>
+          <p>
+            I'm Matheus Raduan, a Frontend Developer based in Brazil 🇧🇷 and for
+            7 years I've been solving problems by design and building web
+            experiences.
+          </p>
+          <p>
+            I have experience since working with digital agency (audaz), passing
+            by a brazilian big tech product company as iFood (group Movile), and
+            with huge client to CI&T, attending EMEA market (Europe, the Middle
+            East and Africa).
+          </p>
+          <p>
+            The main stack that I’ve been worked is React, Next.js, Gatsby,
+            Vue.js, Typescript, React Query, CSS-in-JS, Jest (TDD), Material UI,
+            Contentful and designing REST APIs.
+          </p>
+          <p>
+            Recently I've been focusing on Clean Architecture for the frontend
+            environment.
+          </p>
+          <p>Nice to meet you! 😄</p>
+          <h1>A little bit about my history</h1>
+          <p>I like design.</p>
+          <p> I grew up drawing a lot, since when I was a kid.</p>
+          <p>
+            In the highschool I almost reprove an specific class because I only
+            draw in the school 😅. But don't take me wrong! Give me a chance, I
+            faced this seriously, I was decided to be a illustrator or work with
+            design. For the good, some results start to show earlier.
+          </p>
+          <p>
+            When I was coursing the techinician degree of informatics (in my
+            high school), because the fame of my illustrations one of my arts
+            was ellected to be launched as a t-shirt print to a digital company
+            called Camiseteria (if it were in english would be something like
+            T-shirtting ).
+          </p>
+          <p>
+            This was a great achievement to me, I was only with 15 years and
+            some people would buy a t-shirt with my illustration 😀. So I
+            decided to focus only on illustration and study design, and even
+            started a Visual Communication technical course that contains the
+            core of a Graphic Design degree, how I liked that course.
+          </p>
+          <S.IllustrationWrapper className="img-border">
+            <Image
+              src="/images/eye-of-truth.png"
+              layout="fill"
+              className="img"
+            />
+          </S.IllustrationWrapper>
+          <S.FigCaption>
+            The T-shirt origial Illustration called Eye Of Truth.
+          </S.FigCaption>
+          <S.HistoryContentWrapper>
+            <S.HistoryTextWrapper>
+              <h2>The game change</h2>
+              <p>
+                As I mentioned, in that time I already coursed my techinician
+                degree of informatics. <br /> So I was open too to work with
+                some tech stuff, and because of great friendship of a brother of
+                the church that influenced me a lot, I started to create
+                websites and work as freelance to him for one entire year.{' '}
+                <br /> It was awesome and a great experience. He really invested
+                in my carrer.
+              </p>
+              <p>
+                That was the way that found the place where I could put the two
+                things that I like working together, <b>coding</b> and
+                <b> design</b>. <br /> I found what I wanted to work and live 😄
+              </p>
+            </S.HistoryTextWrapper>
+            <S.UnblackMetalPicWrapper>
+              <S.UnblackMetalPic>
+                <Image
+                  src="/images/adm-server.png"
+                  layout="fill"
+                  className="img"
+                  alt="Two guys in the wood praising God"
+                />
+              </S.UnblackMetalPic>
+              <S.FigCaption>
+                My brother in Christ Diego Neves at left and me at right in
+                2017. Both like death and unblack metal. The phrase in the
+                picture is in norwegian language and said ”If not to adore You
+                for what I was born”.
+              </S.FigCaption>
+            </S.UnblackMetalPicWrapper>
+          </S.HistoryContentWrapper>
+          <h1>How I face the work</h1>
+          <p>
+            I believe that we always work for someone and the Person I work for
+            is bigger than my own work. The question you should ask me is, who
+            do you work for?
+          </p>
+        </S.AboutContentWrapper>
+      </S.AboutMeContent>
     </Layout>
   )
 }
--- src/pages/about/styled.tsx
+++ src/pages/about/styled.tsx
@@ -5,7 +5,7 @@ import media from 'styled-media-query'
 export const Wrapper = styled.section`
   background-color: ${theme.colors.black};
   position: relative;
-  height: calc(100vh - 150px);
+  min-height: calc(100vh - 190px);
 `
 
 export const ImgWrapper = styled.div`
@@ -14,11 +14,13 @@ export const ImgWrapper = styled.div`
   position: absolute;
   top: 0;
   left: 0;
+
   ${media.greaterThan('large')`
     max-width: 30vw;
     left: inherit;
     right: 0;
   `}
+
   .aboutMe__img {
     object-fit: cover;
     object-position: center;
@@ -37,6 +39,7 @@ export const ImgWrapper = styled.div`
       #000000 25.44%,
       rgba(0, 0, 0, 0) 71.28%
     );
+
     ${media.greaterThan('large')`
       display: none;
   `}
@@ -51,6 +54,7 @@ export const PageTitle = styled.h2`
   z-index: 1;
   position: relative;
   padding: 28px 0 0 20px;
+
   ${media.greaterThan('large')`
     font-size: 1.6rem;
     padding: 52px 0 0 60px;
@@ -66,6 +70,7 @@ export const TitleWrapper = styled.div`
   ${media.greaterThan('medium')`
     width: 490px;
   `}
+
   ${media.greaterThan('large')`
     width: max-content;
     /* margin-left: 70px; */
@@ -79,6 +84,7 @@ export const TextWrapper = styled.div`
   position: absolute;
   bottom: 56px;
   width: 100%;
+
   ${media.greaterThan('large')`
     bottom: inherit;
     top: 22vh;
@@ -90,9 +96,11 @@ export const Title = styled.h1`
   font-size: 2.8rem;
   font-weight: 300;
   line-height: 1.2em;
+
   ${media.greaterThan('medium')`
     font-size: 4.2rem;
   `}
+
   ${media.greaterThan('large')`
     font-size: 6.2rem;
   `}
@@ -106,6 +114,7 @@ export const Caption = styled.span`
   margin: 1.2rem 0 0 6rem;
   font-size: 1.2rem;
   display: block;
+
   ${media.greaterThan('large')`
     font-size: 2.2rem;
     margin-top: 2.2rem;
@@ -116,8 +125,137 @@ export const Subtitle = styled.span`
   font-size: 0.9rem;
   display: block;
   margin: 16px 0 0 20px;
+
   ${media.greaterThan('large')`
     font-size: 1.2rem;
     margin-top: 70px;
+  `}
+`
+
+export const AboutMeContent = styled.section`
+  background-color: ${theme.colors.black};
+  width: 100%;
+  margin-top: 2px;
+
+  ${media.greaterThan('large')`
+    padding-top: 100px;
+    margin-top: 3px;
+  `}
+
+  .img {
+    display: block;
+    object-fit: cover;
+    object-position: center center;
+  }
+
+  .img-border {
+    border: solid 1px ${theme.colors.white};
+  }
+`
+
+export const AboutContentWrapper = styled.div`
+  width: calc(100% - 40px);
+  display: block;
+  margin: 0 auto;
+  max-width: 700px;
+  padding-bottom: 100px;
+
+  h1 {
+    font-family: ${theme.font.serif};
+    font-size: 3.2rem;
+    font-weight: 300;
+    line-height: 1em;
+    margin: 56px 0 32px 0;
+
+    ${media.greaterThan('large')`
+      font-size: 4rem;
+    `}
+  }
+
+  h2 {
+    margin: 56px 0 32px 0;
+    font-size: 2.4rem;
+    font-weight: bold;
+  }
+
+  p {
+    font-family: ${theme.font.normal};
+    font-size: 1.8rem;
+    color: #ffffff;
+    line-height: 1.5em;
+    margin: 16px 0 0;
+
+    ${media.greaterThan('large')`
+      font-size: 2rem;
+    `}
+  }
+`
+
+export const Quote = styled.q`
+  padding: 24px 0;
+  color: ${theme.colors.white};
+  font-family: ${theme.font.serif};
+  font-style: italic;
+  font-size: 1.8rem;
+  text-align: center;
+  display: block;
+
+  ${media.greaterThan('large')`
+    padding: 56px 0;
+    font-size: 2.8rem;
+  `}
+`
+
+export const IllustrationWrapper = styled.figure`
+  width: 100%;
+  height: 115px;
+  position: relative;
+  margin-top: 32px;
+
+  ${media.greaterThan('large')`
+    height: 246px;
+  `}
+`
+
+export const FigCaption = styled.figcaption`
+  display: block;
+  font-size: 1.2rem;
+  margin-top: 0.8rem;
+  line-height: 1.5em;
+  max-width: 386px;
+  margin: 0 auto;
+`
+
+export const HistoryContentWrapper = styled.div`
+  ${media.greaterThan('large')`
+    display: flex;
+  `}
+`
+
+export const HistoryTextWrapper = styled.div`
+  ${media.greaterThan('large')`
+     width: 292px;
+  `}
+`
+
+export const UnblackMetalPicWrapper = styled.div`
+  ${media.greaterThan('large')`
+     width: 386px;
+     margin-left: 2.2rem;
+     margin-top: 56px;
+  `}
+`
+
+export const UnblackMetalPic = styled.figure`
+  position: relative;
+  width: 100%;
+  height: 405px;
+  margin-top: 2.2rem;
+  .img {
+    object-fit: contain;
+  }
+
+  ${media.greaterThan('large')`
+    height: 489px;
   `}
 `
