d1d915591053790baf9a59fee839d770c0c18585
Matheus Raduan <matheusraduan@Matheuss-MacBook-Air.local>
2023-12-13 01:19:10 -0300

    chore: add progress bar

--- src/pages/_app.page.jsx
+++ src/pages/_app.page.jsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import GlobalStyles from 'src/styles/global'
 import Head from 'next/head'
+import NextNProgress from 'nextjs-progressbar'
 import { FirstRenderProvider } from '../context/first-render'
 
 function App({ Component, pageProps }) {
@@ -10,14 +11,24 @@ function App({ Component, pageProps }) {
         <title>Matheus Raduan - raduan.me</title>
         <link rel="shortcut icon" href="favicon.png" />
         <link rel="apple-touch-icon" href="favicon.png" />
+        <meta name="theme-color" content="#000000"></meta>
         <meta
           name="description"
-          content="I'm Matheus Raduan. Frontend Developer "
+          content="Personal website of Matheus Raduan, a frontend developer"
         />
-        <meta name="theme-color" content="#000000"></meta>
       </Head>
       <GlobalStyles />
       <FirstRenderProvider>
+        <NextNProgress
+          color="linear-gradient( 90deg,#BC5A55 0%,#D49C73 74.48% )"
+          startPosition={0.3}
+          stopDelayMs={200}
+          height={4}
+          showOnShallow={true}
+          options={{
+            showSpinner: false,
+          }}
+        />
         <Component {...pageProps} />
       </FirstRenderProvider>
     </>
